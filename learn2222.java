21e50cee51d600d22b2536cced153c11dae1fc24
LiGouDan <1729375118@qq.com>
2020-10-08 22:44:56 +0800

    '第一次提交'

--- learn2222.java
+++ learn2222.java
@@ -2,6 +2,9 @@ public class Test{
   public static void main(String[] args){
 
   Sysout.out.pringlt("你好世界2222222222");
+    Sysout.out.pringlt("你好世界2222222222");
+    Sysout.out.pringlt("你好世界2222222222");
+    Sysout.out.pringlt("你好世界2222222222");
   
   }
  
