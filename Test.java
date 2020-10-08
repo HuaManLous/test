4d3a8f63a768715a56d88d2dcc0c885524ad93cb
LiGouDan <1729375118@qq.com>
2020-10-09 00:36:28 +0800

    22222

--- Test.java
+++ Test.java
@@ -2,6 +2,8 @@ public class Test{
   public static void main(String[] args){
 
   Sysout.out.pringlt("你好世界");
+    Sysout.out.pringlt("你好世界");
+    Sysout.out.pringlt("你好世界");Sysout.out.pringlt("你好世界");
   
   }
  
