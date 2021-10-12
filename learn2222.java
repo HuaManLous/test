154587286a5b41e51f0f8c34aec361b9f8798e3c
花满楼 <1729375118@qq.com>
2021-10-12 17:10:43 +0800

    fix 测试mac连接git

--- learn2222.java
+++ learn2222.java
@@ -1,10 +1,10 @@
 public class Test{
   public static void main(String[] args){
 
-  Sysout.out.pringlt("你好世界2222222222");
-    Sysout.out.pringlt("你好世界2222222222");
-    Sysout.out.pringlt("你好世界2222222222");
-    Sysout.out.pringlt("你好世界2222222222");
+  Sysout.out.pringlt("你好世界2222222222333");
+    Sysout.out.pringlt("你好世界222222222332");
+    Sysout.out.pringlt("你好世界222222222233");
+    Sysout.out.pringlt("你好世界2222222222333");
   
   }
  
